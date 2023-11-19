9ae5d937940accb90d2e33501f5e898b9bb13e1f
Johannes <johannes-programming@posteo.org>
2023-11-19 12:51:58 +0100

    Version 0.2.10

--- README.rst
+++ README.rst
@@ -15,3 +15,9 @@ To install lib_dzne_math, you can use `pip`. Open your terminal and run:
 .. code-block:: bash
 
     pip install lib_dzne_math
+
+Links
+-----
+
+* `Download <https://pypi.org/project/lib-dzne-math/#files>`_
+* `Source <https://github.com/johannes-programming/lib_dzne_math>`_
--- src/lib_dzne_math/numerals/__init__.py
+++ src/lib_dzne_math/numerals/__init__.py
@@ -1,14 +1,6 @@
+from openpyxl.utils import get_column_letter as _get_column_letter
 from roman import toRoman as roman
 
 
 def alpha(number):
-    ascii_uppercase = "".join(chr(n) for n in range(0x41, 0x5B))
-    digits = 1
-    while number >= (26 ** digits):
-        number -= (26 ** digits)
-        digits += 1
-    ans = ""
-    while len(ans) < digits:
-        number, r = divmod(number, 26)
-        ans = chr(0x41 + r) + ans
-    return ans 
+    return _get_column_letter(number + 1)
